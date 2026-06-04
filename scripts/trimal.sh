fa1f7a17899eeef8267aa5d58cb5b5c8a489791e
Eranga-Witharana <erangav8@gmail.com>
2026-06-04 14:06:24 +0900

    Update trimal.sh

--- scripts/trimal.sh
+++ scripts/trimal.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# trimal.sh
+#
+# Purpose:
+#   Trim individual CDS alignments using trimAl with
+#   the automated1 algorithm.
+#
+# Input:
+#   *.fa alignment files.
+#
+# Output:
+#   *.fa.fasta trimmed alignments.
+#
+# Usage:
+#   bash trimal.sh
+
 FA=( *.fa )
 
 for fasta in "${FA[@]}"; do

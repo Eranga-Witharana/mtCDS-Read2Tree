0131f5695471c024ff28865d0192b1f00d8efde2
Eranga-Witharana <erangav8@gmail.com>
2026-06-04 14:04:49 +0900

    Update rename.sh

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# rename.sh
+#
+# Purpose:
+#   Rename translated amino acid FASTA files to match
+#   Read2Tree input requirements.
+#
+# Input:
+#   *.aa.fasta
+#
+# Output:
+#   *.fasta
+#
+# Usage:
+#   bash rename.sh
+
 for f in *.aa.fasta; do
     mv "$f" "${f/.aa.fasta/.fasta}"
 done

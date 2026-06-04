f960a0f55dcd11bf69b903e2995a11a199d044a8
Eranga-Witharana <erangav8@gmail.com>
2026-06-04 14:04:18 +0900

    Update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# clean.sh
+#
+# Purpose:
+#   Remove stop codons (*) from translated amino acid
+#   sequences.
+#
+# Input:
+#   aa/*.aa.fasta
+#
+# Output:
+#   aa_clean/*.aa.fasta
+#
+# Usage:
+#   bash clean.sh
+
 mkdir -p aa_clean
 
 for f in aa/*.aa.fasta; do

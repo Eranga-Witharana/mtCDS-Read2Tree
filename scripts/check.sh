cbc8c6e11b92e6904c0de5b53bf14577592ca407
Eranga-Witharana <erangav8@gmail.com>
2026-06-04 14:02:49 +0900

    Update check.sh

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# check.sh
+#
+# Purpose:
+#   Detect duplicate species identifiers within
+#   orthologous group FASTA files.
+#
+# Input:
+#   conserved/*.fasta
+#
+# Output:
+#   Reports duplicated identifiers, if present.
+#
+# Usage:
+#   bash check.sh
+
 for f in conserved/*.fasta; do
 
     dup=$(grep "^>" "$f" | sed 's/>//' | sort | uniq -d)

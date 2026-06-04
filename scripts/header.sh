96e984517e1c5b5b5664320d789fc0d20ce915c8
Eranga-Witharana <erangav8@gmail.com>
2026-06-04 14:05:44 +0900

    Update header.sh

--- scripts/header.sh
+++ scripts/header.sh
@@ -2,6 +2,22 @@ Use the same script for both ogs_aa and ogs_dna.
 
 #!/usr/bin/env bash
 
+# header.sh
+#
+# Purpose:
+#   Append orthologous group (OG) identifiers to FASTA
+#   headers for Read2Tree compatibility.
+#
+# Input:
+#   OG*.fasta
+#
+# Output:
+#   Modified FASTA files with OG identifiers appended
+#   to sequence headers.
+#
+# Usage:
+#   bash header.sh
+
 for f in OG*.fasta; do
 
     og=$(basename "$f" .fasta)

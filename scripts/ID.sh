2d3748f0c962ad5745e0a28c7f7d28e8f479cbbf
Eranga-Witharana <erangav8@gmail.com>
2026-06-04 14:02:18 +0900

    Update ID.sh

--- scripts/ID.sh
+++ scripts/ID.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# ID.sh
+#
+# Purpose:
+#   Replace species names in FASTA headers with unique
+#   species identifiers required by Read2Tree.
+#
+# Input:
+#   conserved/*.fasta
+#   species_map.txt
+#
+# Output:
+#   Updated FASTA files with modified headers.
+#
+# Usage:
+#   bash ID.sh
+
 for f in conserved/*.fasta; do
 awk '
 BEGIN {
